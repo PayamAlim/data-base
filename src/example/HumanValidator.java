3c335860c157d395f45f4b393e6588eeebc327d9
Payam <payam85ar@gmail.com>
2025-04-01 01:44:57 +0330

    Add HumanValidator; An implement of Validator

---
+++ src/example/HumanValidator.java
@@ -0,0 +1,14 @@
+package example;
+
+import db.*;
+import db.exception.InvalidEntityException;
+
+public class HumanValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Human))
+            throw new IllegalArgumentException("The entity is not a human!");
+        if (((Human) entity).name == null || ((Human) entity).age < 0)
+            throw new InvalidEntityException("The human cannot have null name or negative age!");
+    }
+}
