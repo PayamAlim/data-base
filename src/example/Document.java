e75f799413e1a54e39621f065f69994d9043f33d
Payam <payam85ar@gmail.com>
2025-04-01 23:15:32 +0330

    Add Document; An entity which implements Trackable interface

---
+++ src/example/Document.java
@@ -0,0 +1,58 @@
+package example;
+
+import db.*;
+
+import java.util.Date;
+
+public class Document extends Entity implements Trackable {
+    public static final int DOCUMENT_ENTITY_CODE = 37;
+
+    private String content;
+
+    private Date creationDate, lastModificationDate;
+
+    public Document (String content) {
+        this.content = content;
+    }
+
+    @Override
+    public Document copy() {
+        Document copyDocument = new Document(content);
+        copyDocument.id = id;
+
+        return copyDocument;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return DOCUMENT_ENTITY_CODE;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+}
