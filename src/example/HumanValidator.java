19c30838100f36ace554cdc922cf3a65dfc47413
Payam <payam85ar@gmail.com>
2025-04-01 15:03:53 +0330

    Seperate negative age and null name exceptions

--- src/example/HumanValidator.java
+++ src/example/HumanValidator.java
@@ -8,7 +8,9 @@ public class HumanValidator implements Validator {
     public void validate(Entity entity) throws InvalidEntityException {
         if (!(entity instanceof Human))
             throw new IllegalArgumentException("The entity is not a human!");
-        if (((Human) entity).name == null || ((Human) entity).age < 0)
-            throw new InvalidEntityException("The human cannot have null name or negative age!");
+        if (((Human) entity).name == null)
+            throw new InvalidEntityException("The human cannot have null name");
+        if (((Human) entity).age < 0)
+            throw new InvalidEntityException("The human cannot have negative age!");
     }
 }
