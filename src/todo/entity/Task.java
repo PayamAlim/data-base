1bca051f680502511a36ab31defa8a5f1458a49d
Payam <payam85ar@gmail.com>
2025-04-04 19:21:06 +0330

    Add todo package and it's subpackages & basic Task class

---
+++ src/todo/entity/Task.java
@@ -0,0 +1,53 @@
+package todo.entity;
+
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class Task extends Entity implements Trackable {
+    public static final int TASK_ENTITY_CODE = 20;
+
+    private enum Status {
+        NotStarted,
+        InProgress,
+        Completed;
+    }
+
+    public String title, description;
+
+    public Date dueDate;
+    private Date creationDate, lastModificationDate;
+
+    public Status status;
+
+    @Override
+    public Entity copy() {
+        return null;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return TASK_ENTITY_CODE;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+}
