f68981cb3ba32bf4497fed9dc3dae034e985f17e
Payam <payam85ar@gmail.com>
2025-04-11 22:53:30 +0330

    Add InputMisMatch exception

--- src/Main.java
+++ src/Main.java
@@ -15,168 +15,195 @@ public class Main {
         Database.registerValidator(Step.STEP_ENTITY_CODE, new StepValidator());
 
         var scn = new Scanner(System.in);
-        String operation;
+        String operation = "";
         do {
-            operation = scn.next();
+            try {
+                System.out.print("Operation: ");
+                operation = scn.next();
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input!");
+                scn.nextLine();
+                continue;
+            }
+
             if (operation.equals("add")) {
-                String object = scn.nextLine();
+                String object;
+                try {
+                    object = scn.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println("Wrong input!");
+                    scn.nextLine();
+                    continue;
+                }
+
                 if (object.equals(" task")) {
-                    System.out.print("Title: ");
-                    String title = scn.nextLine();
-
-                    System.out.print("Description: ");
-                    String description = scn.nextLine();
-
-                    System.out.print("Due Date (yyyy-mm-dd): ");
-                    String date = scn.nextLine();
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-
-                    Date dueDate = null;
                     try {
-                        dueDate = formatter.parse(date);
+                        System.out.print("Title: ");
+                        String title = scn.nextLine();
+
+                        System.out.print("Description: ");
+                        String description = scn.nextLine();
+
+                        System.out.print("Due Date (yyyy-mm-dd): ");
+                        String date = scn.nextLine();
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+                        Date dueDate = formatter.parse(date);
+
+                        TaskService.saveTask(title, description, dueDate);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Wrong input!");
+                        scn.nextLine();
                     } catch (ParseException e) {
                         System.out.println("Invalid date format");
                         return;
                     }
+                } else if (object.equals(" step")) {
+                    try {
+                        System.out.print("TaskID: ");
+                        int taskId = scn.nextInt();
 
-                    TaskService.saveTask(title, description, dueDate);
-                }
-                else if (object.equals(" step")) {
-                    System.out.print("TaskID: ");
-                    int taskId = scn.nextInt();
+                        scn.nextLine();
+                        System.out.print("Title: ");
+                        String title = scn.nextLine();
 
-                    scn.nextLine();
-                    System.out.print("Title: ");
-                    String title = scn.nextLine();
-
-                    StepService.saveStep(taskId, title);
-                }
-                else {
+                        StepService.saveStep(taskId, title);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Wrong input!");
+                        scn.nextLine();
+                    }
+                } else {
                     System.out.println("There is no such object!");
                 }
-            }
-
-            else if (operation.equals("delete")) {
-                scn.nextLine();
-                System.out.print("ID: ");
-                int id = scn.nextInt();
-
-                Entity entity = null;
+            } else if (operation.equals("delete")) {
                 try {
-                    entity = Database.get(id);
-                }
-                catch (EntityNotFoundException e) {
-                    System.out.println("Cannot delete entity with ID = " + id + ".\n Error: " + e.getMessage());
+                    scn.nextLine();
+                    System.out.print("ID: ");
+                    int id = scn.nextInt();
+
+                    Entity entity = Database.get(id);
+
+                    if (entity instanceof Task)
+                        TaskService.removeTask(id);
+                    else
+                        StepService.removeStep(id);
+
+                    scn.nextLine();
+                } catch (EntityNotFoundException e) {
+                    System.out.println("Cannot delete entity with the given ID.\nError: " + e.getMessage());
                     return;
+                } catch (InputMismatchException e) {
+                    System.out.println("Wrong input!");
+                    scn.nextLine();
+                }
+            } else if (operation.equals("update")) {
+                String object;
+                try {
+                    object = scn.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println("Wrong input!");
+                    scn.nextLine();
+                    continue;
                 }
 
-                if (entity instanceof Task)
-                    TaskService.removeTask(id);
-                else
-                    StepService.removeStep(id);
-
-                scn.nextLine();
-            }
-
-            else if (operation.equals("update")) {
-                String object = scn.nextLine();
                 if (object.equals(" task")) {
-                    System.out.print("ID: ");
-                    int id = scn.nextInt();
+                    try {
+                        System.out.print("ID: ");
+                        int id = scn.nextInt();
 
-                    System.out.print("Field: ");
-                    scn.nextLine();
-                    String field = scn.nextLine();
+                        System.out.print("Field: ");
+                        scn.nextLine();
+                        String field = scn.nextLine();
 
-                    System.out.print("New Value ((yyyy-mm-dd) format for due date): ");
-                    String newValue = scn.nextLine();
+                        System.out.print("New Value ((yyyy-mm-dd) format for due date): ");
+                        String newValue = scn.nextLine();
 
-                    if (field.equals("title"))
-                        TaskService.setTitle(id, newValue);
-                    else if (field.equals("description"))
-                        TaskService.setDescription(id, newValue);
-                    else if (field.equals("status")) {
-                        if (newValue.equals("NotStarted")) TaskService.setAsNotStarted(id);
-                        if (newValue.equals("InProgress")) TaskService.setAsInProgress(id);
-                        if (newValue.equals("Completed")) TaskService.setAsCompleted(id);
-                    }
-                    else if (field.equals("dueDate")) {
-                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-
-                        Date newDueDate = null;
-                        try {
-                            newDueDate = formatter.parse(newValue);
-                        } catch (ParseException e) {
-                            System.out.println("Invalid date format");
-                            return;
+                        if (field.equals("title"))
+                            TaskService.setTitle(id, newValue);
+                        else if (field.equals("description"))
+                            TaskService.setDescription(id, newValue);
+                        else if (field.equals("status")) {
+                            if (newValue.equals("NotStarted")) TaskService.setAsNotStarted(id);
+                            if (newValue.equals("InProgress")) TaskService.setAsInProgress(id);
+                            if (newValue.equals("Completed")) TaskService.setAsCompleted(id);
+                        } else if (field.equals("dueDate")) {
+                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                            Date newDueDate = formatter.parse(newValue);
+                            TaskService.setDueDate(id, newDueDate);
+                        } else {
+                            System.out.println("This field doesn't exist or is not accessible.");
                         }
-
-                        TaskService.setDueDate(id, newDueDate);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Wrong input!");
+                        scn.nextLine();
+                    } catch (ParseException e) {
+                        System.out.println("Invalid date format");
+                        return;
                     }
-                    else {
-                        System.out.println("This field doesn't exist or not accessible");
+                } else if (object.equals(" step")) {
+                    try {
+                        System.out.print("ID: ");
+                        int id = scn.nextInt();
+                        scn.nextLine();
+
+                        System.out.print("Field: ");
+                        String field = scn.nextLine();
+
+                        System.out.print("New Value: ");
+                        String newValue = scn.nextLine();
+
+                        if (field.equals("title"))
+                            StepService.setTitle(id, newValue);
+                        else if (field.equals("status")) {
+                            if (newValue.equals("NotStarted")) StepService.setAsNotStarted(id);
+                            if (newValue.equals("Completed")) StepService.setAsCompleted(id);
+                        } else {
+                            System.out.println("This field doesn't exist or is not accessible.");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Wrong input!");
+                        scn.nextLine();
                     }
-                }
-
-                else if (object.equals(" step")) {
-                    System.out.print("ID: ");
-                    int id = scn.nextInt();
-                    scn.nextLine();
-
-                    System.out.print("Field: ");
-                    String field = scn.nextLine();
-
-                    System.out.print("New Value: ");
-                    String newValue = scn.nextLine();
-
-                    if (field.equals("title"))
-                        StepService.setTitle(id, newValue);
-                    else if (field.equals("status")) {
-                        if (newValue.equals("NotStarted")) StepService.setAsNotStarted(id);
-                        if (newValue.equals("Completed")) StepService.setAsCompleted(id);
-                    }
-                    else {
-                        System.out.println("This field doesn't exist or not accessible");
-                    }
-                }
-
-                else
+                } else {
                     System.out.println("There is no such object!");
-            }
-
-            else if (operation.equals("get")) {
-                String obj = scn.nextLine();
+                }
+            } else if (operation.equals("get")) {
+                String obj;
+                try {
+                    obj = scn.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println("Wrong input!");
+                    scn.nextLine();
+                    continue;
+                }
 
                 ArrayList<Entity> tasks = Database.getAll(Task.TASK_ENTITY_CODE);
 
-                tasks.sort(Comparator.comparing(entity -> ((Task)entity).dueDate));
+                tasks.sort(Comparator.comparing(entity -> ((Task) entity).dueDate));
 
                 if (obj.equals(" task-by-id")) {
-                    System.out.print("ID: ");
-                    int id = scn.nextInt();
-                    scn.nextLine();
+                    try {
+                        System.out.print("ID: ");
+                        int id = scn.nextInt();
+                        scn.nextLine();
 
-                    TaskService.printTask(id);
-                }
-
-                else if (obj.equals(" all-tasks")) {
-                    for (Entity task: tasks)
+                        TaskService.printTask(id);
+                    } catch (InputMismatchException e) {
+                        System.out.println("Wrong input!");
+                        scn.nextLine();
+                    }
+                } else if (obj.equals(" all-tasks")) {
+                    for (Entity task : tasks)
                         TaskService.printTask(task.id);
-                }
-
-                else if (obj.equals("incomplete-tasks")) {
-                    for (Entity task: tasks)
+                } else if (obj.equals("incomplete-tasks")) {
+                    for (Entity task : tasks)
                         if (((Task) task).status != Task.Status.Completed)
                             TaskService.printTask(task.id);
-                }
-
-                else
+                } else {
                     System.out.println("There is no such object!");
-            }
-
-            else if (operation.equals("exit"))
+                }
+            } else if (operation.equals("exit"))
                 System.out.println("Successfully logged out");
-
             else
                 System.out.println("There is no such operation!");
         } while (!operation.equals("exit"));
